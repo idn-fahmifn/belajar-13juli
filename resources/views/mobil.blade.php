fe33f6dad116acf2ab7d3d712c24f09c8cb2b689
idn-fahmifn <fahmifn.idn@gmail.com>
2026-07-13 14:56:15 +0700

    1.3

--- resources/views/mobil.blade.php
+++ resources/views/mobil.blade.php
@@ -9,5 +9,53 @@
     <h1>Hallo ini adalah halaman mobil.</h1>
     <p>detail lengkap mobil, <a href="{{route('halaman.detail', 'avanza')}}">avanza</a> </p>
     <p>detail lengkap mobil, <a href="{{route('mobil.sport')}}">Sport</a> </p>
+
+    <form action="{{route('proses.mobil')}}" method="post">
+        @csrf
+        <div class="">
+            <label for="">nama mobil</label>
+            <input type="text" name="name" required>
+            @error('name')
+                <span style="color: #ff0000;">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="">
+            <label for="">tipe mobil</label>
+            <input type="text" name="tipe" required>
+            @error('tipe')
+                <span style="color: #ff0000;">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="">
+            <label for="">warna</label>
+            <input type="text" name="warna" required>
+            @error('warna')
+                <span style="color: #ff0000;">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="">
+            <label for="">kapasitas bahan bakar</label>
+            <input type="number" name="bahan_bakar" required>
+            @error('bahan_bakar')
+                <span style="color: #ff0000;">{{$message}}</span>
+            @enderror
+        </div>
+        
+        <div class="">
+            <label for="">Jenis Mobil</label>
+            <select name="jenis" id="jenis" required>
+                <option value="suv">SUV</option>
+                <option value="electric">electric</option>
+                <option value="hybrid">hybrid</option>
+            </select>
+            @error('jenis')
+                <span style="color: #ff0000;">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="">
+            <button type="submit">Kirim data</button>
+        </div>
+    </form>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,14 +19,42 @@ Route::get('/mobil', function () {
 Route::middleware('login')->group(function () {
 
     Route::get('/mobil/sport', function () {
-        return "Ini adlaah sport";
+        return 'Ini adlaah sport';
     })->name('mobil.sport');
 
-    Route::get('/mobil/{brandtype}', function () {
-        return view('detail');
-    })->name('halaman.detail');
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/mobil/{brandtype}', function () {
+            return view('detail');
+        })->name('halaman.detail');
+    });
 
 });
+
+Route::post('/proses', function(Request $request){
+
+    $request->validate([
+        'name' => ['required', 'min:5', 'max:10', 'string'],
+        'tipe' => ['required', 'min:5', 'max:10', 'string'],
+        'warna' => ['required', 'in:merah,silver,hitam'],
+        'bahan_bakar' => ['required', 'integer', 'min:1', 'max:150'],
+        'jenis' => ['required', 'in:suv,electric,hybrid'],
+    ], [
+        'name.required' => 'Input ini wajib diisi',
+        'name.min' => 'masukan minimal 5 karakter',
+        'name.max' => 'masukan maksimal 10 karakter',
+        'name.string' => 'Input berbentuk karakter atau kata',
+
+        'tipe.required' => 'Input ini wajib diisi',
+        'tipe.min' => 'masukan minimal 5 karakter',
+        'tipe.max' => 'masukan maksimal 10 karakter',
+        'tipe.string' => 'Input berbentuk karakter atau kata',
+
+        'warna.required' => 'Input ini wajib diisi',
+        'warna.in' => 'Input yang diizinkan (merah, silver dan hitam)',
+    ]);
+
+})->name('proses.mobil');
+
 
 // shortcut menampikan (testing halaman) - menampilkan halaman statis
 Route::view('tampilan', 'welcome');
